5b063d5a6c4fad13aa08201db7a4b34563d6e5b4
redlatrix <mariacamila0103@hotmail.com>
2026-05-14 16:57:23 -0500

    fix(env): se rcorrigen archivos de configuración

--- src/services/coreApi.ts
+++ src/services/coreApi.ts
@@ -22,11 +22,6 @@ coreApi.interceptors.request.use(
 coreApi.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
-    }
     return Promise.reject(error);
   }
 );
